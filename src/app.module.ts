b0235e923595e35c0b701d44dbbb5a1d5b5584a2
marefati110 <marefati110@gmail.com>
2021-11-11 21:12:32 +0330

    add user and auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './app/auth/auth.module';
+import { UserModule } from './app/user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: '127.0.0.1',
+        port: 5432,
+        username: 'postgres',
+        password: 'postgres',
+        autoLoadEntities: true,
+        database: 'jirjirak',
+        entities: ['dist/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+    }),
+    AuthModule,
+    UserModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
